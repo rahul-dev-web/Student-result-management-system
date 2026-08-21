3bac9cc15ebf109a65c5d3fa770acc71f19b6361
Rahul Kumar Jha <jarahul989@gmail.com>
2026-08-21 23:30:38 +0530

    Clear stateless student auth cookie on logout

--- student/logout.php
+++ student/logout.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+require_once __DIR__ . '/../includes/auth.php';
+clear_auth_cookie('student');
 unset($_SESSION['student']);
-header('Location: ../login.php');
+header('Location: /login.php');
 exit;
